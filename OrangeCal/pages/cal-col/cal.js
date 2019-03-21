1da2fca67d28dd25eb0ec3431a43229a19295b0b
Oppaju <48217187+Oppaju@users.noreply.github.com>
2019-03-21 19:20:09 +0800

    Add files via upload

---
+++ OrangeCal/pages/cal-col/cal.js
@@ -0,0 +1,90 @@
+Page({
+  data:{
+    id0:"0",
+    id1:"1",
+    id2:"2",
+    id3:"3",
+    id4:"4",
+    id5:"5",
+    id6:"6",
+    id7:"7",
+    id8:"8",
+    id9:"9",
+    idpoint:".",
+    idac:"ac",
+    idper:"%",
+    idadd:"+",
+    idsub:"-",
+    idmul:"*",
+    iddiv:"/",
+    iddel:"del",
+    idequal:"="
+  },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+   btnClick: function (e) {
+    //获取点击事件响应的id
+    console.log(e);
+
+  },
+})
